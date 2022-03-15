d5bebadd89bd60b76717438f0c06b854681ce062
apicazorla <76844926+apicazorla@users.noreply.github.com>
2022-03-15 23:16:20 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { createAppContainer } from "react-navigation";
+import { createMaterialBottomTabNavigator } from
+    "react-navigation-material-bottom-tabs";
+  
+import HomeScreen from "./screens/HomeScreen";
+import UserScreen from "./screens/UserScreen";
+import SettingScreen from "./screens/SettingScreen";
+  
+const TabNavigator = createMaterialBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarLabel: "Home",
+        tabBarIcon: (tabInfo) => (
+          <Ionicons
+            name="md-home"
+            size={tabInfo.focused ? 26 : 20}
+            color={tabInfo.tintColor}
+          />
+        ),
+      },
+    },
+    User: {
+      screen: UserScreen,
+      navigationOptions: {
+        tabBarLabel: "User",
+        tabBarIcon: (tabInfo) => (
+          <Ionicons
+            name="md-person-circle-outline"
+            size={tabInfo.focused ? 26 : 20}
+            color={tabInfo.tintColor}
+          />
+        ),
+      },
+    },
+    Setting: {
+      screen: SettingScreen,
+      navigationOptions: {
+        tabBarLabel: "Setting",
+        tabBarIcon: (tabInfo) => (
+          <Ionicons
+            name="md-settings-outline"
+            size={tabInfo.focused ? 26 : 20}
+            color={tabInfo.tintColor}
+          />
+        ),
+      },
+    },
+  },
+  {
+    initialRouteName: "Home",
+    barStyle: { backgroundColor: "#006600" },
+  }
+);
+  
+const Navigator = createAppContainer(TabNavigator);
+  
+export default function App() {
+  return (
+    <Navigator>
+     <HomeScreen/>
+    </Navigator>
+  );
+}
